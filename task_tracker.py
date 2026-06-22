98122db226b2471ed46cf2bc2d5be286eee87bb5
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-06-22 16:05:11 +0530

    added id and description and file

--- task_tracker.py
+++ task_tracker.py
@@ -1,15 +1,34 @@
+import json
+import datetime
+import uuid as u
+
 # Defining a function called task_tracker inside that code all the properties goes.
 def task_tracker():
-    tasks = []
+    try:
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+    except FileNotFoundError:
+        tasks = [
+            {
+                "Id": id,
+                "description": task_description
+                # "Status":
+                # "createdAt":
+                # "updatedAt":
+            }
+        ]
 # Using while loop to run the program infinite time until the user give valid command
     while True:
         print("-----Task Tracker CLI-----")
         for i,task in enumerate(tasks, 1):
             print(f"{i} {task}")
 # Inside "choose" variable user will enter the command they want to perform.
-        choose = input(f"What you like to do: add | update(Task Number) | done(e.g., Done1) | delete(e.g., delete 1) | Quit: ").strip().lower()
+        choose = input(f"What you like to do: add | update(Task Number) | done(e.g., done 1) | delete(e.g., delete 1) | Quit: ").strip().lower()
+# If user input is "add" code below will input user for a task then append that task to the tasks list.
         if choose == "add":
             task = input("Task: ")
+            task_description = input("Task discription: ")
+            id = u.uuid4(task)
             if task:
                 tasks.append(task)
                 print("Task Added!")
@@ -39,5 +58,7 @@ def task_tracker():
             break
         else:
             print("Invalid Command")
+    with open("tasks.json", "w") as file:
+        json.dump(tasks,file)
 
 task_tracker()
