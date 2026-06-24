81468d75afb3fe2b607972a5eb635edf8b986295
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-06-24 16:50:13 +0530

    Half way through adding CLI functionallity

--- task_tracker.py
+++ task_tracker.py
@@ -1,9 +1,14 @@
 import json
 from datetime import datetime
 import uuid as u
+import argparse
 
 # Defining a function called task_tracker inside that function all the code goes.
 def task_tracker():
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command")
+    add_parser = subparsers.add_parser("add")
 
     def save_task_file(tasks):
         with open("tasks.json", "w") as file:
@@ -14,70 +19,65 @@ def task_tracker():
             tasks = json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         tasks = []
-# Using while loop to run the program infinite time until the user give valid command
-    while True:
-        print("-----Task Tracker CLI-----")
-        for i,task in enumerate(tasks, 1):
-            print(f"{i} {task}")
+    print("-----Task Tracker CLI-----")
+    for i,task in enumerate(tasks, 1):
+        print(f"{i} {task}")
 # Inside "choose" variable user will enter the command they want to perform.
-        choose = input(f"What you like to do: add | Task in Progress(e.g., progress 1) | update(e.g., update 1) | done(e.g., done 1) | delete(e.g., delete 1) | Quit: ").strip().lower()
-# If user input is "add" code below will input user for a task then append that task to the tasks list.
-        if choose == "add":
-            task = input("Task: ")
-            id = str(u.uuid4())
-            status = "todo"
-            createdAt = datetime.now().strftime("%Y-%m-%d %H:%M")
-            updatedAt = datetime.now().strftime("%Y-%m-%d %H:%M")
-            task_data = {
-                "Id": id,
-                "Description": task,
-                "Status": status,
-                "CreatedAt": createdAt,
-                "UpdatedAt": updatedAt
-            }
-            if task:
-                tasks.append(task_data)
-                print("Task Added!")
+    # If user input is "add" code below will input user for a task then append that task to the tasks list.
+    if command == "add":
+        task = input("Task: ")
+        id = str(u.uuid4())
+        status = "todo"
+        createdAt = datetime.now().strftime("%Y-%m-%d %H:%M")
+        updatedAt = datetime.now().strftime("%Y-%m-%d %H:%M")
+        task_data = {
+            "Id": id,
+            "Description": task,
+            "Status": status,
+            "CreatedAt": createdAt,
+            "UpdatedAt": updatedAt
+        }
+        if task:
+            tasks.append(task_data)
+            print("Task Added!")
+            save_task_file(tasks)
+    elif command == "progress":
+        try:
+            num = int(args.task_num) - 1
+            if 0 <= num < len(tasks):
+                task_progress = "In Progress"
+                tasks[num]["Status"] = task_progress
                 save_task_file(tasks)
-        elif choose.startswith("progress"):
-            try:
-                num = int(choose.split()[1]) - 1
-                if 0 <= num < len(tasks):
-                    task_progress = "In Progress"
-                    tasks[num]["Status"] = task_progress
-                    save_task_file(tasks)
-            except IndexError:
-                print("Invalid Task Number")
-        elif choose.startswith("update"):
-            try:
-                num = int(choose.split()[1]) - 1
-                if  0 <= num < len(tasks):
-                    updated_task = input(f"Update Task {num + 1}: ")
-                    updated_time = datetime.now().strftime("%Y-%m-%d %H:%M")
-                    tasks[num]["Description"] = updated_task
-                    tasks[num]["UpdatedAt"] = updated_time
-                    save_task_file(tasks)
-            except IndexError:
-                print("Invalid Task Number")
-        elif choose.startswith("done"):
-            try:
-                num = int(choose.split()[1]) -1
-                if 0 <= num < len(tasks):
-                    done_task = ("Done")
-                    tasks[num]["Status"] = done_task
-                    save_task_file(tasks)
-            except IndexError:
-                print("Invalid Task Number")
-        elif choose.startswith("delete"):
-            try:
-                num = int(choose.split()[1]) - 1
-                tasks.pop(num)
+        except IndexError:
+            print("Invalid Task Number")
+    elif command == "update":
+        try:
+            num = int(args.task_num) - 1
+            if  0 <= num < len(tasks):
+                updated_task = input(f"Update Task {num + 1}: ")
+                updated_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+                tasks[num]["Description"] = updated_task
+                tasks[num]["UpdatedAt"] = updated_time
                 save_task_file(tasks)
-            except IndexError:
-                print("Invalid Task Number")
-        elif choose == "quit":
-            break
-        else:
-            print("Invalid Command")
+        except IndexError:
+            print("Invalid Task Number")
+    elif command == "done":
+        try:
+            num = int(args.task_num) -1
+            if 0 <= num < len(tasks):
+                done_task = ("Done")
+                tasks[num]["Status"] = done_task
+                save_task_file(tasks)
+        except IndexError:
+            print("Invalid Task Number")
+    elif command == "delete":
+        try:
+            num = int(args.task_num) - 1
+            tasks.pop(num)
+            save_task_file(tasks)
+        except IndexError:
+            print("Invalid Task Number")
+    else:
+        print("Invalid Command")
 
 task_tracker()
